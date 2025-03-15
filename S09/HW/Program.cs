62cb516d39e9ea8fe659e07799a316a8bde5eef9
parnia <parniafazeli2005@gmail.com>
2025-03-15 08:06:24 -0700

    register student

---
+++ S09/HW/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+namespace HW;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("please enter name");
+            string Studentname = Console.ReadLine();
+            Console.WriteLine("please enter stdid");
+            int studentid = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter natid");
+            int nationalid = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter the Credites");
+            int studentcredites = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter the activity");
+            bool activity = bool.Parse(Console.ReadLine());
+
+
+            Student s = new Student(Studentname, studentid, nationalid, studentcredites, activity);
+            File.AppendAllText("student.csv", s.ToString() + Environment.NewLine);
+            Console.WriteLine("Student added successfully!");
+
+            
+
+        }   
+    }
+}
